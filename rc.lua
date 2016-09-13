2fb572b770775af64039c24fd362c5f3d7442599
Dmitriy Sharavyov <dimsharav@gmail.com>
2016-09-14 03:41:34 +0500

    Убрана зависимость от wmii

--- rc.lua
+++ rc.lua
@@ -475,4 +475,4 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
-awful.util.spawn_with_shell("~/.wmii/autostart")
+awful.util.spawn_with_shell("~/autostart")
