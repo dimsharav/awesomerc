2ce8ac0a683cb1c116bda6ffa2b6a11ddd0a4bfc
Dmitriy Sharavyov <dimsharav@gmail.com>
2016-09-14 02:15:36 +0500

    Добавлены правила для приложений

--- rc.lua
+++ rc.lua
@@ -381,13 +381,15 @@ awful.rules.rules = {
                      buttons = clientbuttons } },
     { rule = { class = "MPlayer" },
       properties = { floating = true } },
-    { rule = { class = "pinentry" },
-      properties = { floating = true } },
-    { rule = { class = "gimp" },
-      properties = { floating = true } },
-    -- Set Firefox to always map on tags number 2 of screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { tag = tags[1][2] } },
+    -- Start apps on defined tags
+    { rule = { class = "terminal" },
+      properties = { tag = tags[1][1] } },
+    { rule = { class = "Firefox" },
+      properties = { tag = tags[1][4] } },
+    { rule = { class = "Telegram" },
+      properties = { tag = tags[1][5] } },
+    { rule = { class = "Thunar" },
+      properties = { tag = tags[1][7] } },
 }
 -- }}}
 
